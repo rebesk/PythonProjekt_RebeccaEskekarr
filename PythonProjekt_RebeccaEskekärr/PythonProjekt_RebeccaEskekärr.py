761a61def21cb9ae163bbd6115a0032d99ac3337
Rebecca Eskekärr <rebecca@eskekarr.se>
2023-05-23 17:08:52 +0200

    start pygame-set up

--- PythonProjekt_RebeccaEskekärr/PythonProjekt_RebeccaEskekärr.py
+++ PythonProjekt_RebeccaEskekärr/PythonProjekt_RebeccaEskekärr.py
@@ -1,1 +1,18 @@
+import pygame
+from sys import exit
 
+#run pygame
+pygame.init()
+#create a screen
+screen = pygame.display.set_mode((800,600))
+
+#game loop
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            exit()
+
+pygame.exit()
