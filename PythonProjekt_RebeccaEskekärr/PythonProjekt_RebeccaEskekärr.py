5f0b2b6f05b106b8a35ba818e5541a45ff8c3b2f
Rebecca Eskekärr <rebecca@eskekarr.se>
2023-05-24 14:48:24 +0200

    Start av bakgrund för spelet

--- PythonProjekt_RebeccaEskekärr/PythonProjekt_RebeccaEskekärr.py
+++ PythonProjekt_RebeccaEskekärr/PythonProjekt_RebeccaEskekärr.py
@@ -4,7 +4,10 @@ from sys import exit
 #run pygame
 pygame.init()
 #create a screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800,570))
+
+#Background
+background = pygame.image.load('gamebackground.png')
 
 #title and icon
 pygame.display.set_caption('Jumping over') # Initial name of game
@@ -16,6 +19,8 @@ pygame.display.set_icon(icon)
 running = True
 while running:
 
+    
+    screen.blit(background, (0,0)) # material av blit https://dr0id.bitbucket.io/legacy/pygame_tutorial01.html 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
