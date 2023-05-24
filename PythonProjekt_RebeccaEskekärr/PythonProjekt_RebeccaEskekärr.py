edc815970404ef9826d5e8e40e7fd1b7f6308d56
Rebecca Eskekärr <rebecca@eskekarr.se>
2023-05-24 21:08:18 +0200

    Tar allt ihop till main-method och utgår därifrån

--- PythonProjekt_RebeccaEskekärr/PythonProjekt_RebeccaEskekärr.py
+++ PythonProjekt_RebeccaEskekärr/PythonProjekt_RebeccaEskekärr.py
@@ -6,21 +6,38 @@ pygame.init()
 #create a screen
 screen = pygame.display.set_mode((800,600))
 
+#Background
+background = pygame.image.load('gamebackground.png')
+
 #title and icon
 pygame.display.set_caption('Jumping over') # Initial name of game
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
+#Player
+playerImage = pygame.image.load('warrior.png')
+playerX = 200
+playerY = 380
+
+
+def player(x, y):
+    screen.blit(playerImage, (x, y)) #draw player on image
 
 #game loop
 running = True
 while running:
 
+
+    
+    screen.blit(background, (0,0)) # material av blit https://dr0id.bitbucket.io/legacy/pygame_tutorial01.html 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             exit()
 
+
+
+    player(playerX, playerY) #calling player
     pygame.display.update()
 
 pygame.exit()
